4f9faf7e14fc4e142a076117f059aa01a05f57b1
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-02-28 12:20:29 +0100

    SzarazTropusi class kész

---
+++ allatokClass/SzarazTropusi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allatokClass
+{
+	internal class SzarazTropusi
+	{
+		private string nev;
+		private string faj;
+		private int seb;
+		private int suly;
+
+		public SzarazTropusi(string nev, string faj, int seb, int suly)
+		{
+			this.nev = "Leon S. Kennedy";
+			this.faj = "jaguár";
+			this.seb = 80;
+			this.suly = 75;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public string Faj { get => faj; set => faj = value; }
+		public int Seb { get => seb; set => seb = value; }
+		public int Suly { get => suly; set => suly = value; }
+	}
+}
