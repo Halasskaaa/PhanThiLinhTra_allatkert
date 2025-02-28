30ef9402f5a357bf3f13afa63271de8f052da4c8
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-02-28 12:17:29 +0100

    SzarazMedi class kész

---
+++ allatokClass/SzarazMeleg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allatokClass
+{
+	internal class SzarazMeleg
+	{
+		private string nev;
+		private string faj;
+		private int seb;
+		private int suly;
+
+		public SzarazMeleg(string nev, string faj, int seb, int suly)
+		{
+			this.nev = "Albert Wesker";
+			this.faj = "strucc";
+			this.seb = 70;
+			this.suly = 110;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public string Faj { get => faj; set => faj = value; }
+		public int Seb { get => seb; set => seb = value; }
+		public int Suly { get => suly; set => suly = value; }
+	}
+}
