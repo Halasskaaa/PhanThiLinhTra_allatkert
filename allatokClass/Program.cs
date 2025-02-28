783db9a5296f2843548f60335835b3d3577d6e88
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-02-28 11:22:32 +0100

    Szazalek() és Verseny() metódus mentése

--- allatokClass/Program.cs
+++ allatokClass/Program.cs
@@ -4,7 +4,49 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Szazalek();
+			Verseny();
 		}
+
+		static void Verseny()
+		{
+            Console.WriteLine("0\n0");
+			int haladas = 0;
+			int haladas2 = 0;
+			for (int i = 0; i < 5; i++)
+			{
+				Thread.Sleep(1000);
+                Console.Clear();
+				Console.SetCursorPosition(haladas += 2, 0);
+                Console.WriteLine("0");
+				Console.SetCursorPosition(haladas2 += 3, 1);
+                Console.WriteLine("0");
+            }
+		}
+		static void Szazalek()
+		{
+			string[] szavak = new string[5];
+			szavak[0] = "alma";
+			szavak[1] = "béla";
+			szavak[2] = "cecil";
+
+            Console.WriteLine("_______");
+            Console.Write("|");
+
+			for (int i = 0;i < szavak.Length;i++)
+			{
+				if (szavak[i] != null) Console.BackgroundColor = ConsoleColor.Green;
+				else Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write(" ");
+            }
+			Console.BackgroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("|");
+			Console.WriteLine("_______");
+
+		}
+
+
+
 	}
 }
