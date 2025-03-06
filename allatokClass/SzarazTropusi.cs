02216f983bdc2bef6e8244e83dc9fbe9b89d3cc4
Halasskaaa <phanthilinhtra25@gmail.com>
2025-03-06 21:06:58 +0100

    Update SzarazTropusi.cs
    javítás, ugyanaz mint ViziAllat classnál

--- allatokClass/SzarazTropusi.cs
+++ allatokClass/SzarazTropusi.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace allatokClass
 {
-	internal class SzarazTropusi
+	internal class SzarazTropusi : IAllat
 	{
-		private string nev;
-		private string faj;
-		private int seb;
-		private int suly;
+		public string nev { get; set; }
+        	public string faj { get; set; }
+	        public int seb { get; set; }
+        	public int suly { get; set; }
 
 		public SzarazTropusi()
 		{
@@ -21,10 +21,6 @@ namespace allatokClass
 			suly = 75;
 		}
 
-		public string Nev { get => nev; set => nev = value; }
-		public string Faj { get => faj; set => faj = value; }
-		public int Seb { get => seb; set => seb = value; }
-		public int Suly { get => suly; set => suly = value; }
 		public override string ToString()
 		{
 			return $"Az állat neve: {nev}, faja: {faj}, sebessége: {seb}, súlya: {suly}";
