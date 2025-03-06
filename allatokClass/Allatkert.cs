27f7e025b3cd63c9defe57278d211e42d8f74ff2
Halasskaaa <phanthilinhtra25@gmail.com>
2025-03-06 20:45:49 +0100

    Update Allatkert.cs
    Verseny metódus itt lett elkészítve.
    Más állatok accessibilitása, félig kész. (a többi állatos classokba be kell állítani a get, set-et)

--- allatokClass/Allatkert.cs
+++ allatokClass/Allatkert.cs
@@ -7,49 +7,77 @@ using System.Threading.Tasks;
 
 namespace allatokClass
 {
-	internal class Allatkert
-	{
-		
-		static List<int> ListSeb()
-		{
-			ViziAllat delfin = new();
-			SzarazHideg jszarvas = new();
-			SzarazMedi vaddiszno = new();
-			SzarazMeleg strucc = new();
-			SzarazTropusi jaguar = new();
-			
-			List<int> sebLista = new List<int>() { delfin.Seb, jszarvas.Seb, vaddiszno.Seb, strucc.Seb, jaguar.Seb  };
+    internal class Allatkert
+    {
+        private List<IAllat> allatLista;
+        private Random random;
 
-            return sebLista;
-		}
-		static List<string> ListFaj()
-		{
-			ViziAllat delfin = new();
-			SzarazHideg jszarvas = new();
-			SzarazMedi vaddiszno = new();
-			SzarazMeleg strucc = new();
-			SzarazTropusi jaguar = new();
+        public Allatkert()
+        {
+            random = new Random();
+            int allatSzam = random.Next(5, 16);
+            allatLista = new List<IAllat>();
 
-			List<string> fajLista = new List<string>() { delfin.Faj, jszarvas.Faj, vaddiszno.Faj, strucc.Faj, jaguar.Faj };
+            for (int i = 0; i < allatSzam; i++)
+            {
+                switch (random.Next(5))
+                {
+                    case 0:
+                        allatLista.Add(new ViziAllat());
+                        break;
+                    case 1:
+                        allatLista.Add(new SzarazHideg());
+                        break;
+                    case 2:
+                        allatLista.Add(new SzarazMedi());
+                        break;
+                    case 3:
+                        allatLista.Add(new SzarazMeleg());
+                        break;
+                    case 4:
+                        allatLista.Add(new SzarazTropusi());
+                        break;
+                }
+            }
+        }
 
-			return fajLista;
-		}
-		static List<string> ListNev()
-		{
-			ViziAllat delfin = new();
-			SzarazHideg jszarvas = new();
-			SzarazMedi vaddiszno = new();
-			SzarazMeleg strucc = new();
-			SzarazTropusi jaguar = new();
+        public void Verseny()
+        {
+            Console.WriteLine("Verseny indul!");
 
-			List<string> nevLista = new List<string>() { delfin.Nev, jszarvas.Nev, vaddiszno.Nev, strucc.Nev, jaguar.Nev };
+            List<int> positions = new List<int>(new int[allatLista.Count]);
 
-			return nevLista;
-		}
+            int maxSteps = 10;
+            for (int step = 0; step < maxSteps; step++)
+            {
+                Console.Clear();
 
+                for (int i = 0; i < allatLista.Count; i++)
+                {
+                    positions[i] += allatLista[i].Seb;
+                }
 
+                foreach (var animal in allatLista)
+                {
+                    int position = positions[allatLista.IndexOf(animal)];
+                    Console.SetCursorPosition(0, allatLista.IndexOf(animal));
+                    Console.WriteLine($"{animal.Nev}: " + new string('O', position));
+                }
 
+                Thread.Sleep(500);
+            }
 
+            int maxPosition = positions.Max();
+            int winnerIndex = positions.IndexOf(maxPosition);
+            Console.WriteLine("\nA verseny győztese: " + allatLista[winnerIndex].Nev);
+        }
 
-	}
+        public void AllatokInfo()
+        {
+            foreach (var allat in allatLista)
+            {
+                Console.WriteLine(allat);
+            }
+        }
+    }
 }
